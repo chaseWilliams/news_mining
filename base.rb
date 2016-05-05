fcfb8a141e5d95b11148dd0d104c489975e010f6
Chase Williams <bornofawesomeness@gmail.com>
2016-05-05 14:23:11 +0000

    added wikipedia validation

--- base.rb
+++ base.rb
@@ -1,26 +1,34 @@
 require 'rest-client'
-require 'json_color'
+require 'json'
 require 'rss'
 require './lib/k-gram.rb'
 require './lib/array_extension.rb'
-slack_url = "https://hooks.slack.com/services/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"
 
-payload = {
-    'username' => "Feedr",
-    'icon_emoji' => ":gem:",
-    'text' => "This is a test:\n#{@title}\n#{@link}"
-}.to_json
-
-
+hash = Hash.new
 file = IO.read './test_txt'
 gram = Gram.new(file, 2)
-print gram.n_gram
-puts "\n"
 print gram.tagged
 puts "\n"
-#begin
-#  RestClient.post slack_url, payload, content_type: 'application/json'
-#rescue RestClient::InternalServerError => e
-#  "500 error"
-#  puts e.response
-#end
+gram.tagged.each do |key, value|
+    str = key[0].capitalize + "_" + key[1]
+    endpoint_base = 'https://en.wikipedia.org/w/api.php?action=query&'
+    endpoint_article = 'prop=extracts&format=json&titles='
+    uri = endpoint_base + endpoint_article + "#{str}"
+    response = JSON.parse RestClient::Request.execute(:url => URI.encode(uri), :method => :get, :verify_ssl => false)
+    begin 
+        hash_keys = response['query']['pages'].keys
+        result = response['query']['pages'][hash_keys[0]]['extract']
+        index = result.index '</p>'
+        extract = result.slice 0, index
+        hash[key] = {
+            url: uri,
+            extract: extract
+        }
+    rescue NoMethodError => e
+        puts "looks like #{uri} doesn't exist in Wikipedia"
+    end
+end
+
+puts "final result is #{JSON.pretty_generate hash}"
+
+# persist hash to database
